14aee752b3ec786e917353498b43ab8a2af27b6a
kamilquliyev98 <kamilag@code.edu.az>
2021-12-03 03:40:10 +0400

    Employee Salary

---
+++ EmployeeSalary/EmployeeSalary/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeSalary
+{
+    class Employee
+    {
+        public string Name;
+        public string Surname;
+        public string Position;
+        public int DailySalary;
+        public int Salary;
+
+        public void MonthlySalary(string Name, string Surname, string Position, int DailySalary)
+        {
+            this.Name = Name;
+            this.Surname = Surname;
+            this.Position = Position;
+            this.DailySalary = DailySalary;
+            int Salary = DailySalary * 30;
+
+            Console.WriteLine($"{Name} {Surname} {Position} {Salary}");
+        }
+    }
+}
---
+++ EmployeeSalary/EmployeeSalary/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EmployeeSalary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+
+            employee.MonthlySalary("Kamil", "Guliyev", "Developer", 50);
+        }
+    }
+}
