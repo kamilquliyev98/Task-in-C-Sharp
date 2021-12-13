4df8a7b3844979079f0663222273375bd1708100
kamilquliyev98 <kamilag@code.edu.az>
2021-12-14 02:07:28 +0400

    Calculator Library

---
+++ CalculatorApp/CalculatorApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using Library;
+using System;
+
+namespace CalculatorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+            double result = calc.Calc(0.5, '/', 0.05);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ CalculatorApp/Library/Calculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Library
+{
+    public class Calculator
+    {
+        public double result { get; set; }
+
+        public double Calc(double x, char op, double y)
+        {
+            switch (op)
+            {
+                case '+':
+                    result = x + y;
+                    break;
+                case '-':
+                    result = x - y;
+                    break;
+                case '*':
+                case 'x':
+                    result = x * y;
+                    break;
+                case '/':
+                case ':':
+                    result = x / y;
+                    break;
+                default:
+                    result = 0;
+                    break;
+            }
+            return result;
+        }
+    }
+}
