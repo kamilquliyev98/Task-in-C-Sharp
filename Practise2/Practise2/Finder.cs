18c6079311e15ae60028c6075cd5f03fb208ef83
Kamil Guliyev <94452586+kamilquliyev98@users.noreply.github.com>
2021-12-20 03:34:14 +0400

    Practise2

---
+++ Practise2/Practise2/Finder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practise2
+{
+    class Finder
+    {
+        public void Find(int[] arr)
+        {
+            int min = arr[0];
+            int index = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] <= min)
+                {
+                    min = arr[i];
+                    index = i;
+                }
+            }
+
+            Console.WriteLine($"Arrayin en kicik elementi: {min}\nIndex: {index}");
+        }
+    }
+}
---
+++ Practise2/Practise2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Practise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { -1, 0, 1, -12, 3, 4, 5, 6, 7, 8, 9, -10, -2 };
+
+            Finder find = new Finder();
+            find.Find(array);
+        }
+    }
+}
